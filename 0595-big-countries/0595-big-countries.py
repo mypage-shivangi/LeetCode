75515ae6275422e097a9af275b48479fd6677702
mypage-shivangi <35975585+mypage-shivangi@users.noreply.github.com>
2025-08-10 19:05:26 +0530

    Time: 280 ms (41.84%), Space: 68.6 MB (85.38%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    #df=pd.DataFrame(world)
-    mask = (world["area"] >=3000000) | (world["population"] >=25000000)
-    return world.loc[mask,["name","population","area"]]
+    area = world["area"].to_numpy()
+    pop = world["population"].to_numpy()
+    mask = (area >= 3000000) | (pop >= 25000000)
+    return world.loc[mask, ["name", "population", "area"]]
