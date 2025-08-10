29f42558a0af40a574dae398641ef2e26459c20f
mypage-shivangi <35975585+mypage-shivangi@users.noreply.github.com>
2025-08-11 02:13:59 +0530

    Time: 452 ms (39.6%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+    RETURN (
+        /* Write your T-SQL query statement below. */
+       SELECT TOP 1 salary
+       FROM (
+            SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rank
+            FROM Employee
+            ) AS RankedSalaries
+        WHERE rank = @N
+         
+
+    );
+END
