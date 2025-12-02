a8acd92ef2a8e08293186e524c6508bdaf081214
mypage-shivangi <35975585+mypage-shivangi@users.noreply.github.com>
2025-12-02 21:22:52 +0530

    Time: 286 ms (50.04%), Space: 67.7 MB (15.55%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.py
+++ 1148-article-views-i/1148-article-views-i.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    auth=views["author_id"].to_numpy()
-    v_id=views["viewer_id"].to_numpy()
-    mask=(auth==v_id)
-    return pd.DataFrame({"id":np.sort(views.loc[mask,"author_id"].unique())})
+    #auth=views["author_id"].to_numpy()
+    #v_id=views["viewer_id"].to_numpy()
+    #mask=(auth==v_id)
+    #return pd.DataFrame({"id":np.sort(views.loc[mask,"author_id"].unique())})
+    df=views[views['author_id']==views['viewer_id']]
+    return df[['author_id']] \
+        .drop_duplicates() \
+        .rename(columns={'author_id': 'id'}) \
+        .sort_values('id', ascending=True)
