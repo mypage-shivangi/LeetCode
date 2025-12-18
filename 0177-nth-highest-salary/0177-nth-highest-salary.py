61132e973895b813a34624d0994c78563211f50d
mypage-shivangi <35975585+mypage-shivangi@users.noreply.github.com>
2025-12-18 20:39:08 +0530

    Time: 314 ms (7.6%), Space: 67.2 MB (45.57%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.py
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
-    distinct_salaries = employee['salary'].drop_duplicates().sort_values(ascending=False).reset_index(drop=True)
-    if N >0 and N<= len(distinct_salaries):
-        result = distinct_salaries.iloc[N-1]
-    else:
-        result = np.nan
-    return pd.DataFrame({f'getNthHighestSalary({N})': [result]})
+    employee['rank'] = employee['salary'].rank(method='dense', ascending=False)
+    result=employee.loc[employee['rank']==N,['salary']]
+    if result.empty:
+        return pd.DataFrame({f'getNthHighestSalary({N})': [None]})
+    
+    return pd.DataFrame({f'getNthHighestSalary({N})': result.iloc[0]})
