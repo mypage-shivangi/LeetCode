ff1b6d805d13addd1b536644a60a2913e7692b51
mypage-shivangi <35975585+mypage-shivangi@users.noreply.github.com>
2025-12-22 19:38:08 +0530

    Time: 295 ms (87.58%), Space: 69.4 MB (14.93%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -1,21 +1,13 @@
-import pandas as pd
-
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
     if employee.empty:
-       return pd.DataFrame(columns=['Department', 'Employee', 'salary'])
-    # Calculate rank (dense_rank equivalent) of salaries within each department
+        return pd.DataFrame(columns=['Department', 'Employee', 'salary'])
+    
     employee['dense_rank'] = employee.groupby('departmentId')['salary'] \
                                     .rank(method='dense', ascending=False)
-    
-    # Filter to only highest salary (rank = 1)
     top_salaries = employee[employee['dense_rank'] == 1].copy()
-    
-    # Join with department on departmentId
-    result = pd.merge(department, top_salaries, left_on='id', right_on='departmentId', how='left')
-    
-    # Select columns you want to return, e.g. department name, employee name, salary
-    result= result[['name_x', 'name_y', 'salary']].rename(columns={
+    result = pd.merge(department[['id', 'name']], top_salaries, left_on='id', right_on='departmentId', how='inner')
+    result = result[['name_x', 'name_y', 'salary']].rename(columns={
         'name_x': 'Department',
         'name_y': 'Employee'
     })
-    return result
+    return result.sort_values(by='Employee').reset_index(drop=True)
