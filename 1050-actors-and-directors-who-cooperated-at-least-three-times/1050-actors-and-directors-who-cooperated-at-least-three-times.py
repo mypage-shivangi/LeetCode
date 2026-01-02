09e78c34c7c3134a3408aa534b0c409cb11f028d
mypage-shivangi <35975585+mypage-shivangi@users.noreply.github.com>
2026-01-02 15:26:25 +0530

    Time: 280 ms (63.56%), Space: 67.8 MB (43.15%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    return (
+        actor_director
+        .groupby(['actor_id','director_id'])
+        .size()
+        .reset_index(name='cnt')
+        .query('cnt>=3')
+        [['actor_id','director_id']])
